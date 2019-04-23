cec13ef8545faf266d49edda14cd50c60d202918
Damian Kapka <damiankapka@live.com>
2019-04-23 23:38:52 +0200

    Register Form with validation

--- src/views/RegisterForm.vue
+++ src/views/RegisterForm.vue
@@ -1,10 +1,85 @@
 <template>
-     <h1>REGISTER</h1>
+     <v-form ref="form" v-model="valid">
+          <v-container>
+               <v-layout>
+                    <v-flex xs8 offset-xs2>
+                         <v-text-field v-model="FName" :rules="FNameRules" label="Imię" required/>
+                         <v-text-field v-model="LName" :rules="LNameRules" label="Nazwisko" required/>
+                         <v-text-field v-model="Password" :rules="PasswordRules" label="Hasło" required/>
+                         <v-text-field v-model="PasswordRepeat" :rules="PasswordRepeatRules" label="Powtórz hasło" required/>
+                         <v-text-field v-model="Mail" :rules="MailRules" label="Adres e-mail" required/>
+                         <v-text-field v-model="Phone" :rules="PhoneRules" label="Nr telefonu"/>
+                         <v-layout row>
+                              <v-flex xs1></v-flex>
+                              <v-flex xs4><v-btn block color="success" v-on:click="submit()">Stwórz Konto</v-btn></v-flex>
+                              <v-flex xs2></v-flex>
+                              <v-flex xs4><v-btn block color="warning" v-on:click="reset()">Reset</v-btn></v-flex>
+                              <v-flex xs1></v-flex>
+                         </v-layout>
+                    </v-flex>
+               </v-layout>
+          </v-container>
+     </v-form>
 </template>
 
 <script>
 export default {
+     name: 'RegisterForm',
 
+     data() {
+          return{
+          valid: false,
+          FName: '',
+          FNameRules:[
+               f => !!f || "Wprowadz swoje imię",
+               f => /^[a-zA-Z]{3,}$/.test(f) || "Imie powinno zawierac conajmniej 3 znak bez spacji, znaków specjalnych ani liczb"
+          ],
+          LName: '',
+          LNameRules:[
+               l => !!l || "Wprowadz swoje nazwisko",
+               l => /^[a-zA-Z]{3,}$/.test(l) || "Nazwisko powinno zawierac conajmniej 3 znak bez spacji, znaków specjalnych ani liczb"
+          ],
+          Password: '',
+          PasswordRules: [
+               p => !!p || "Wprowadź hasło",
+               p => /^.*(?=.{4,32})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/.test(p) || "Hasło musi zawierać duże i małe litery, cyfry oraz znaki specjalne"
+          ],
+          PasswordRepeat: '',
+          PasswordRepeatRules: [
+               r => !!r || "Powtórz hasło",
+               r => r == this.Password || "Hasła nie są takie same"
+          ],
+          Mail: '',
+          MailRules: [
+               m => !!m || "Wprowadź adres e-mail",
+               m => /^.+\@\w+\.\w+$/.test(m) || "Format adresu email niepoprawny"
+          ],
+          Phone: '',
+          PhoneRules:[
+               ph => /^\d{3}\-\d{3}\-\d{3}$/.test(ph) || "Wpisz numer telefonu w formacie 123-123-123"
+          ],
+          FullName: function(){
+               return this.FName + " " + this.LName
+          }
+          }
+     },
+     watch: {
+
+     },
+     methods: {
+          reset(){
+               this.$refs.form.reset();
+          },
+          submit(){
+               alert(this.FullName())
+               if(this.$refs.form.validate()){
+                    
+               }
+               else{
+                    alert('bad');
+               }
+          }
+     },
 }
 </script>
 
