0a5a1830a8f78c173d90b12f97ae0f9d1ad4166e
Damian Kapka <Damian Kapka@327-DKAPKA>
2019-05-23 16:34:43 +0200

    CreateNewTournament form draft

--- src/views/Profile/NewTournament.vue
+++ src/views/Profile/NewTournament.vue
@@ -2,10 +2,57 @@
   <v-flex xs8 offset-xs2>
     <v-form ref="form" v-model="valid" style="padding: 5%">
       <v-text-field
-        v-model="name"
-        label="Nazwa turnieju"
+        v-model="Name"
+        label="Nazwa"
+        :rules="NameRules"
         required
       ></v-text-field>
+      <v-combobox
+        label="Dyscyplina"
+        v-model="Discipline"
+        :items="DisciplineOptions"        
+      >
+      </v-combobox>
+
+      <v-menu
+        ref="menu"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        :return-value.sync="StartingDate"
+        lazy
+        transition="scale-transition"
+        offset-y
+        full-width
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+            v-model="StartingDate"
+            label="Data rozpoczecia turnieju"
+            readonly
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="StartingDate" no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+          <v-btn flat color="primary" @click="$refs.menu.save(StartingDate)">OK</v-btn>
+        </v-date-picker>
+      </v-menu> 
+
+      <v-text-field 
+        v-model="AmountOfTeams" 
+        label="Ilość dryżun" 
+        :rules="AmountOfTeamsRules" 
+        required>
+      </v-text-field>
+
+      <v-text-field 
+        v-model="EntryFee" 
+        label="Wpisowe" 
+        :rules="EntryFeeRules" 
+        required a>
+      </v-text-field>
     </v-form>
   </v-flex>
 </template>
@@ -16,7 +63,28 @@ export default {
   data() {
     return {
       valid: false,
-      name: ""
+      Name: "",
+      NameRules: [
+          n => !!n || "Wpisz nazwe turnieju",
+          n => /^.{5,}$/.test(n.trim()) || "Nazwa turnieju powinna zawierać conajmniej 5 znaków"
+      ],
+      Discipline: "Piłka Nożna",
+      DisciplineOptions: [
+          "Piłka Nożna",
+          "Koszykówka",
+          "Siatkówka"
+      ],
+      StartingDate: new Date().toISOString().substr(0, 10),
+      AmountOfTeams: "",
+      AmountOfTeamsRules:[
+          a => !!a || "Wprowadz ilość drużyn w turnieju",
+          a => /^[1-9]{1}[0-9]?$/.test(a.trim()) || "Ilośc drużym musi być cyrfą wieksza od 0"
+      ],
+      EntryFee: "",
+      EntryFeeRules:[
+          e => !!e || "Wprowadz wpisowe do turnieju",
+          e => /^[1-9]{1}[0-9]?$/.test(e.trim()) || "Wpisowe musi być cyrfą wieksza od 0"
+      ],
     };
   }
 };
