6f07003557b04ed557d14c19765b6cd4d60a3b93
Damian Kapka <damiankapka@live.com>
2019-10-24 01:46:02 +0200

    MatchDetailsDialog draft v2

--- src/components/MatchDetailsDialog.vue
+++ src/components/MatchDetailsDialog.vue
@@ -1,42 +1,91 @@
 <template>
-    <v-dialog v-model="dialog" max-width="500">
-        <template v-slot:activator="{ on }">
-            <v-icon v-on="on">
-                search
-            </v-icon>
-        </template>
-        <v-card style="padding:10%">
-            <v-card-title class="headline">
-                <v-layout style="text-align: center">
-                    <v-flex xs4 offset-xs1>
-                        {{match.homeTeamName}}
-                    </v-flex>
-                    <v-flex xs2>
-                        {{match.homeTeamPoints}} :  {{match.guestTeamPoints}}
-                    </v-flex>
-                    <v-flex xs4>
-                        {{match.guestTeamName}}
-                    </v-flex>
-                </v-layout>
-            </v-card-title>
-        </v-card>
-    </v-dialog>
+  <v-dialog v-model="dialog" max-width="700">
+    <template v-slot:activator="{ on }">
+      <v-icon v-on="on">
+        search
+      </v-icon>
+    </template>
+    <v-container class="grey lighted-5">
+      <v-layout row>
+        <v-flex xs12>
+          <v-card tile class="pa-2 match-dateTime">
+            <v-icon color="black">alarm</v-icon>
+            <span style="margin-left: 4%;margin-right: 4%;">{{
+              matchDateFormatted
+            }}</span>
+            <v-icon color="black">alarm</v-icon>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex xs4>
+          <v-card tile class="pa-3 match-score-participant">
+            <v-icon color="black" size="34px">people</v-icon>
+            {{ match.homeTeamName }}
+          </v-card>
+        </v-flex>
+        <v-flex xs4>
+          <v-card tile class="pa-2 match-score-score">
+            {{ match.homeTeamPoints }} : {{ match.guestTeamPoints }}
+          </v-card>
+        </v-flex>
+        <v-flex xs4>
+          <v-card tile class="pa-3 match-score-participant">
+            {{ match.guestTeamName }}
+            <v-icon color="black" size="34px">people</v-icon>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex xs12>
+          <v-card class="pa-2 match-scorers-head" tile>
+              Zdobywcy punktów
+          </v-card>
+        </v-flex>
+      </v-layout>
+
+    </v-container>
+  </v-dialog>
 </template>
 
 <script>
-    export default {
-        name: "MatchDetailsDialog",
-        data(){
-            return {
-                dialog:false,
-            }
-        },
-        props:{
-            match:Object
-        }
+export default {
+  name: "MatchDetailsDialog",
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  props: {
+    match: Object
+  },
+  computed: {
+    matchDateFormatted: function() {
+      return this.match.matchDateTime.replace("T", " ");
     }
+  }
+};
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.match-dateTime {
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+}
+.match-score-participant {
+  .match-dateTime();
+  font-size: 34px;
+}
 
+.match-score-score {
+  .match-dateTime();
+  font-size: 45px;
+}
+
+.match-scorers-head {
+  .match-dateTime();
+  font-size: 16px;
+  text-decoration: underline;
+}
 </style>
